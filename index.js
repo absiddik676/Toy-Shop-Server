6346bd0a12fc74876e790fe2361b8b11a55a23bb
absiddik676 <absiddik8200@gmail.com>
2023-05-19 21:53:17 +0600

    toy add in database route added

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const ToysCollection =  client.db('ToysDB').collection('ToysCollection')
+
+    app.post('/addToy',async(req,res)=>{
+        const data = req.body
+        const result = await ToysCollection.insertOne(data)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
